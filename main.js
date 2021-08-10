0681ca6be3a2c166f28aef724e0b0435d1e97c71
sid4803 <84769771+sid4803@users.noreply.github.com>
2021-08-10 13:55:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,107 @@ function preload(){
     img=loadImage("dog_cat.jpg");
 }
 function setup(){
-    canvas=createCanvas(640,420);
-    canvas.center();
+    canvas = createCanvas(380, 380); canvas.center(); video = createCapture(VIDEO); video.size(380,380); video.hide();
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     objectdetector=ml5.objectDetector("cocossd",modelLoaded);
     document.getElementById("status").innerHTML="STATUS:OBJECT DETECTING";
 }
@@ -25,48 +124,9 @@ function gotResult(error,results){
     }
 }
 function draw(){
-    image(img,0,0,640,420);
+   image(video, 0, 0, 380, 380);
     if(status!=""){
-        for(i=0;i<objects.length;i++){
-            document.getElementById("status").innerHTML="STATUS:OBJECT DETECTED";
-            fill("red");
-            percent=floor(objects[i].confidence*100);
-            text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
-            noFill();
-            stroke("red");
-            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
-        }
-    }
-    }
-status="";
-img="";
-objects=[];
-function preload(){
-    img=loadImage("dog_cat.jpg");
-}
-function setup(){
-    canvas=createCanvas(640,420);
-    canvas.center();
-    objectdetector=ml5.objectDetector("cocossd",modelLoaded);
-    document.getElementById("status").innerHTML="STATUS:OBJECT DETECTING";
-}
- function modelLoaded(){
-     console.log("modelLoaded");
-     status=true;
-     objectdetector.detect(img,gotResult);
- }
-function gotResult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        objects=results;
-    }
-}
-function draw(){
-    image(img,0,0,640,420);
-    if(status!=""){
+r = random(255); g = random(255); b = random(255);
         for(i=0;i<objects.length;i++){
             document.getElementById("status").innerHTML="STATUS:OBJECT DETECTED";
             fill("red");
