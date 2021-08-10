63bcb730500a42616e0cef48a2c0ed67334d7496
sid4803 <84769771+sid4803@users.noreply.github.com>
2021-08-10 14:03:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -127,9 +127,10 @@ function draw(){
    image(video, 0, 0, 380, 380);
     if(status!=""){
 r = random(255); g = random(255); b = random(255);
+        objectDetector.detect(video, gotResult);
         for(i=0;i<objects.length;i++){
             document.getElementById("status").innerHTML="STATUS:OBJECT DETECTED";
-            fill("red");
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length; fill(r,g,b);
             percent=floor(objects[i].confidence*100);
             text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
             noFill();
